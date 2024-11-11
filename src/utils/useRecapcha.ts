0e5157ecaed38e48f8e83f67bfd7547e6e6169a3
Austin Howard <cnfishead@hotmail.com>
2024-11-10 20:01:18 -0500

    recaptcha

--- src/utils/useRecapcha.ts
+++ src/utils/useRecapcha.ts
@@ -8,20 +8,19 @@ export const useRecapcha = () => {
   const handleLoaded = (_: any) => {
     (window as any).grecaptcha.ready((_: any) => {
       (window as any).grecaptcha
-        .execute('6Ld98jclAAAAAFcnhWn9ECw39uthjBeoIGqE4VP-', {
+        .execute('6Lfk93oqAAAAAN_X64WuPI2wrI-YbNoTxULAw29f', {
           action: 'login',
         })
         .then(async (token: any) => {
           console.log('token');
-          // const { data } = await axios.post('/auth/recaptcha', {
-          //   token,
-          // });
+          const { data } = await axios.post('/auth/recaptcha', {
+            token,
+          });
 
-          // console.log('Your score', data.score);
-          // if (data.isVerified) {
-          //   setRecapchaIsVerified(true);
-          // }
-          setRecapchaIsVerified(true);
+          console.log('Your score', data.score);
+          if (data.isVerified) {
+            setRecapchaIsVerified(true);
+          }
         });
     });
   };
@@ -30,7 +29,7 @@ export const useRecapcha = () => {
     // Add reCaptcha
     const script = document.createElement('script');
     script.src =
-      'https://www.google.com/recaptcha/api.js?render=6Ld98jclAAAAAFcnhWn9ECw39uthjBeoIGqE4VP-';
+      'https://www.google.com/recaptcha/api.js?render=6Lfk93oqAAAAAN_X64WuPI2wrI-YbNoTxULAw29f';
     script.addEventListener('load', handleLoaded);
     document.body.appendChild(script);
   }, []);
